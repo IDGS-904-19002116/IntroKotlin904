41d1f15dea3f58e5936f82e513e8e481e8df9996
IDGS-904-19002116 <fotos.eurm@gmail.com>
2025-05-06 19:34:07 -0600

    tema3-if-when

--- app/src/main/java/com/example/introkotlin904/TemasKotlin/tema2Collections.kt
+++ app/src/main/java/com/example/introkotlin904/TemasKotlin/tema2Collections.kt
@@ -7,21 +7,29 @@ package com.example.introkotlin904.TemasKotlin
     MutableSet
     Map
     MitableMap
-
-
+*/
+fun main(){
     val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
         println(readOnlyFiguras)
+    println(" La primer figura es ${readOnlyFiguras[0]}")
+    println(" El primer elemento es ${readOnlyFiguras.first()}")
+    println("Número de elementos ${readOnlyFiguras.count()} items")
+    println("Circulo" in readOnlyFiguras)
+    println(readOnlyFiguras)
+    //readOnlyFiguras.add("pentagono")
 
-    val figura: mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
-    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+
+
+    val figura: MutableList<String> = mutableListOf("cuadrado2","triangulo2","circulo2")
+    println(figura)
+    figura.add("pentagono2")
+    println(figura)
+    figura.remove("cuadrado2")
+    println(figura)
+
     val mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
-
     val frutas = setOf("manzana", "bananas", "naranja")
-    val frutas = mutableSetOf("manzana", "bananas", "naranja")
-
-
-
-*/fun main(){
+    val frutas2 = mutableSetOf("manzana", "bananas", "naranja")
     val coches = mapOf("uno" to 1, "dos" to 2, "tres" to 3)
     val coches2 = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3)
     println(coches2)
