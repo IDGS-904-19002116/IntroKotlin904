16a2cfae71f0398ccae53989187af350756a71a3
IDGS-904-19002116 <fotos.eurm@gmail.com>
2025-05-06 19:09:43 -0600

    Tema1 Variables

---
+++ app/src/main/java/com/example/introkotlin904/TemasKotlin/tema2Collections.kt
@@ -0,0 +1,28 @@
+package com.example.introkotlin904.TemasKotlin
+
+/*
+    List
+    Mutablelist
+    Set
+    MutableSet
+    Map
+    MitableMap
+
+
+    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+        println(readOnlyFiguras)
+
+    val figura: mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
+    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+    val mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
+
+    val frutas = setOf("manzana", "bananas", "naranja")
+    val frutas = mutableSetOf("manzana", "bananas", "naranja")
+
+
+
+*/fun main(){
+    val coches = mapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    val coches2 = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    println(coches2)
+}
